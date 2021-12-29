79e1f4bc9d9e50fbaf16d6380730e36234f00c95
Christopher Brewin <cbrewin93@gmail.com>
2021-12-29 18:08:07 +1000

    update

--- Sparky-core/main.cpp
+++ Sparky-core/main.cpp
@@ -1,26 +1,45 @@
 #include "src/graphics/window.h"
 
+void printErrors(std::string s) {
+    GLenum err;
+    int i = 0;
+    while((err = glGetError()) != GL_NO_ERROR)
+    {
+        std::cout << s << err << ", " << glewGetErrorString(err)  << std::endl;
+        i++;
+    }
+
+    if (i == 0) {
+        std::cout << s << " No Errors"  << std::endl;
+    }
+}
 
 int main() {
     using namespace sparky;
     using namespace graphics;
 
     Window window("Sparky!" ,960, 540);
-
     glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
     while (!window.closed()) {
+        printErrors("Start: ");
         window.clear();
+        printErrors("Clear: ");
         glBegin(GL_TRIANGLES);
+        printErrors("Begin: ");
         glVertex2f(-0.5f, -0.5f);
+        printErrors("Vert 1: ");
         glVertex2f(0.0f, 0.5f);
+        printErrors("Vert 2: ");
         glVertex2f(0.5f, -0.5f);
+        printErrors("Vert 3: ");
         glEnd();
+        printErrors("End: ");
+
         window.update();
+        printErrors("Update: ");
+
     }
-
-    glewInit();
-
 
     return 0;
 }
--- Sparky-core/src/graphics/window.cpp
+++ Sparky-core/src/graphics/window.cpp
@@ -28,6 +28,11 @@ namespace sparky::graphics {
             return false;
         }
 
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
         m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, nullptr, nullptr);
 
         if (!m_Window) {
@@ -37,6 +42,14 @@ namespace sparky::graphics {
 
         glfwMakeContextCurrent(m_Window);
         glfwSetWindowSizeCallback(m_Window, windowResize);
+
+        if (glewInit() != GLEW_OK) {
+            std::cout << "Could not initialize GLEW!" << std::endl;
+            return false;
+        }
+
+        std::cout <<  "OPENGL" << glGetString(GL_VERSION) << std::endl;
+
         return true;
     }
 
